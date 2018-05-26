da836bab32ab3cbd4699a18af18e0469c54c87d1
Florian Adonis <florian@Florians-MacBook-Pro.local>
2018-05-27 00:42:35 +0200

    FIX fixed small stuffs

--- webapp-api/index.js
+++ webapp-api/index.js
@@ -6,34 +6,39 @@ const Spinner = Logger.spinner();
 const PeopleController = require('./lib/poi/peopleController');
 
 const setGlobalHeaders = async (req, res, next) => {
-  res.header('Content-Type', 'application/json');
-  next();
+    res.header('Content-Type', 'application/json');
+    next();
 };
 
 const initEndpoints = async () => {
-  let app = express();
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({extended: true}));
+    let app = express();
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
 
-  // GET
-  app.get('/*', setGlobalHeaders);
-  app.get('/people', PeopleController.onGet);
 
-  // POST
-  app.post('/*', setGlobalHeaders);
-  app.post('/people', PeopleController.onPost)
+    ///////// PEOPLE ///////////////
 
-  app.listen(process.env.PORT || 8080);
+    // GET
+    app.get('/*', setGlobalHeaders);
+    app.get('/people', PeopleController.onGet);
+
+    // POST
+    app.post('/*', setGlobalHeaders);
+    app.post('/people', PeopleController.onPost);
+
+    ///////////////////////////////////
+
+    app.listen(process.env.PORT || 8080);
 };
 
 const init = async () => {
-  Logger.info('Initializing app');
-  Spinner.start('Connecting to Database');
-  Spinner.succeed();
-  await PeopleController.init();
-  await initEndpoints();
+    Logger.info('Initializing app');
+    Spinner.start('Connecting to Database');
+    Spinner.succeed();
+    await PeopleController.init();
+    await initEndpoints();
 };
 
 (async () => {
-  await init();
+    await init();
 })();
